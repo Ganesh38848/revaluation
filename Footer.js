339c1a44909fbae60eb4c323ca4326dcc0262919
Ganesh38848 <ganeshnahak907@gmail.com>
2025-06-30 02:07:15 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () =>{
+    return(
+        <footer className="footer">
+            <p> @  2025 your name . all right reserverd</p>
+        </footer>
+    )
+
+}
+ export default Footer;
---
+++ MainSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MainSection = () => {
+    return(
+        <main className= "main-content">
+            <p> this is a simple main section as the design</p>
+        </main>
+    )
+}
+export default MainSection;
